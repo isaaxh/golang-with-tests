42da8bf4ace207cf2af1f826b709cd94cad054c3
Ishaq Hussain (Isaac) <isaaclearns58@gmail.com>
2026-01-28 02:33:49 +0300

    add: add perimeter and area finder functions.

---
+++ structs/shapes_test.go
@@ -0,0 +1,35 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f but want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != want {
+			t.Errorf("got %g but want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{5.0, 2.0}
+		checkArea(t, rectangle, 10)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.1592653589793)
+	})
+
+}
