09c9e2405cf1983003e8abe0a592f4210f2c74af
Ishaq Hussain (Isaac) <isaaclearns58@gmail.com>
2026-01-28 03:42:44 +0300

    add: exercise complete

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,13 +2,15 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
+// Different Shapes
+
 type Rectangle struct {
 	Width  float64
 	Height float64
-}
-
-type Shape interface {
-	Area() float64
 }
 
 func (r Rectangle) Area() float64 {
@@ -23,10 +25,23 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+type Square struct {
+	Side float64
+}
+
+func (s Square) Area() float64 {
+	return s.Side * s.Side
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
